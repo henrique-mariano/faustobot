cf179efa1ab46fc4edd35ddd445c7587c955662d
Haori1 <henriquiui70@gmail.com>
2017-12-07 02:40:00 -0200

    Fotos

--- faustobot/config.js
+++ faustobot/config.js
@@ -1,7 +1,7 @@
 var config = {
-    consumer_key:           'PYIgIxvAYp1bG0fhnXpwQfevR',
-    consumer_secret:        'XRR6QjsY3D5GGFzsJPm9XIcNcN5zhWZnjg1XugSDkgCO06h6M4',
-    access_token:           '923968620325195776-JxMLzMjQjRaxHP0Q5OGZk0Eu9BCVlbV',
-    access_token_secret:    'RkBxsZVbtOa79lWAayLCcwFAOjKo8p2jPglgF5GeXkcTD'
+    consumer_key:           'CONSUMER_KEY',
+    consumer_secret:        'CONSUMER_SECRET',
+    access_token:           'ACCESS_TOKEN',
+    access_token_secret:    'ACCESS_TOKEN_SECRET'
 }
 module.exports = config;
